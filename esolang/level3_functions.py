8385554100b39720142af354003a226af5427799
James <cheeto@Jamess-MacBook-Air-3.local>
2024-11-20 15:28:28 -0800

    last

--- esolang/level3_functions.py
+++ esolang/level3_functions.py
@@ -1,117 +1,115 @@
 import lark
-import pprint
-import esolang.level2_loops
+import esolang.level1_statements
 
 
-grammar = esolang.level2_loops.grammar + r"""
-    %extend start: function_call
-        | function_def
+grammar = esolang.level1_statements.grammar + r"""
+    %extend start: forloop 
+                | whileloop
+                | comparison
+                | range 
 
-    function_def: "lambda" NAME ("," NAME)* ":" start
+    forloop: "for" NAME "in" range block
 
-    ?args_list: start ("," start)*
+    range: "range" "(" start ")"
 
-    function_call: NAME "(" args_list ")"
-        | NAME "(" ")"
+    comparison: start COMPARISON_OPERATOR start
+
+    COMPARISON_OPERATOR: ">" | "<" | ">=" | "<=" | "==" | "!="
+
+    whileloop: "while" comparison block
+
+
 """
 parser = lark.Lark(grammar)
 
 
-class Interpreter(esolang.level2_loops.Interpreter):
+class Interpreter(esolang.level1_statements.Interpreter):
     '''
     >>> interpreter = Interpreter()
-    >>> interpreter.visit(parser.parse("a=3; print(a)"))
-    3
-    >>> interpreter.visit(parser.parse("a=4; b=5; stack()"))
-    [{'a': 4, 'b': 5}]
-    >>> interpreter.visit(parser.parse("a=4; b=5; {c=6}; stack()"))
-    [{'a': 4, 'b': 5}]
-    >>> interpreter.visit(parser.parse("print(10)"))
-    10
-    >>> interpreter.visit(parser.parse("for i in range(10) {print(i)}"))
-    0
-    1
-    2
-    3
-    4
-    5
-    6
-    7
-    8
+    >>> interpreter.visit(parser.parse("for i in range(10) {i}"))
     9
-    >>> interpreter.visit(parser.parse(r"f = lambda x : x; f(5)"))
+    >>> interpreter.visit(parser.parse("a=0; for i in range(10) {a = a + i}"))
+    45
+    >>> interpreter.visit(parser.parse("a=0; for i in range(10) {a = a + i}; a"))
+    45
+    >>> interpreter.visit(parser.parse("a=0; for i in range(10) {a = a + i}; i")) # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+        ...
+    ValueError: Variable i undefined
+
+    >>> interpreter.visit(parser.parse("a=5; for i in range(a) {a = a + i}; a"))
+    15
+    >>> interpreter.visit(parser.parse("a=0; while a < 10 {a = a + 1}"))
+    10
+    >>> interpreter.visit(parser.parse("a=0; while a < 5 {a = a + 1}; a"))
     5
-    >>> interpreter.visit(parser.parse(r"f = lambda x,y : x+y; f(5, 6)"))
-    11
-    >>> interpreter.visit(parser.parse(r"f = lambda x,y,z : x+y-z; f(5, 6, 7)"))
+    >>> interpreter.visit(parser.parse("1 > 0"))
+    True
+    >>> interpreter.visit(parser.parse("0 > 1"))
+    False
+    >>> interpreter.visit(parser.parse("a=0; while a < 6 {a = a + 1}; a"))
+    6
+    >>> interpreter.visit(parser.parse("a=0; while a < 7 {a = a + 3}; a"))
+    9
+    >>> interpreter.visit(parser.parse("a=10; while a > 0 {a = a - 2}; a"))
+    0
+    >>> interpreter.visit(parser.parse("a=1; while a < 4 {a = a * 2}; a"))
     4
+    >>> interpreter.visit(parser.parse("a=0; for i in range(3) {a = a + i}; a"))
+    3
+    >>> interpreter.visit(parser.parse("a=0; while a < 5 {a = a + 2}; a"))
+    6
+    >>> interpreter.visit(parser.parse("a=0; while a < 10 {a = a + 3}; a"))
+    12
+    >>> interpreter.visit(parser.parse("a=5; while a > 3 {a = a - 1}; a"))
+    3
+    >>> interpreter.visit(parser.parse("a=0; while a < 2 {a = a + 1}; a"))
+    2
     '''
-    def __init__(self):
-        super().__init__()
-
-        # we add a new level to the stack
-        # the top-most level will be for "built-in" functions
-        # all lower levels will be for user-defined functions/variables
-        # the stack() function will only print the user defined functions
-        self.stack.append({})
-        self.stack[0]['print'] = print
-        self.stack[0]['stack'] = lambda: pprint.pprint(self.stack[1:])
-        self.stack[0]['is_prime'] = self.is_prime
-        
-    
-    def function_def(self, tree):
-        names = [token.value for token in tree.children[:-1]]
-        body = tree.children[-1]
-        def foo(*args):
-            self.stack.append({})
-            for name, arg in zip(names, args):
-                self._assign_to_stack(name, arg)
-            ret = self.visit(body)
-            self.stack.pop()
-            return ret
-        return foo
-
-    def function_call(self, tree):
-        name = tree.children[0]
-
-        # the tree can be structured in different ways depending on the number of arguments;
-        # the following lines convert the params list into a single flat list
-        params = [self.visit(child) for child in tree.children[1:]]
-        params = [param for param in params if param is not None]
-        if len(params) > 0 and isinstance(params[-1], list):
-            params = params[0]
-
-        return self._get_from_stack(name)(*params)
-
     def range(self, tree):
-        if len(tree.children) == 2:
-            start = self.visit(tree.children[0])
-            end = self.visit(tree.children[1])
-        else:
-            start = 0
-            end = self.visit(tree.children[0])
-        return range(start, end)
-    
-    def is_prime(self, n):
-        """
-        >>> interpreter = Interpreter()
-        >>> interpreter.visit(parser.parse("is_prime(1)"))
-        False
-        >>> interpreter.visit(parser.parse("is_prime(2)"))
-        True
-        >>> interpreter.visit(parser.parse("is_prime(3)"))
-        True
-        >>> interpreter.visit(parser.parse("is_prime(4)"))
-        False
-        """
-        if n < 2:
-            return False
-        if n == 2:
-            return True
-        if n % 2 == 0:
-            return False
-        for divisor in range(3, int(n ** 0.5) + 1, 2):
-            if n % divisor == 0:
-                return False
-        return True
+        if len(tree.children) == 1:
+            stop = self.visit(tree.children[0])
+            return range(0, stop)
 
+        start = self.visit(tree.children[0])
+        stop = self.visit(tree.children[1])
+        return range(start, stop)
+
+
+    def forloop(self, tree):
+        varname = tree.children[0].value
+        loop_range = self.visit(tree.children[1])
+        block = tree.children[2]
+
+        self.stack.append({})
+        result = None
+
+        for value in loop_range:
+            self.stack[-1][varname] = value  
+            result = self.visit(block) 
+        self.stack.pop()
+        return result
+
+    def whileloop(self, tree):
+        result = None
+        while self.visit(tree.children[0]):
+            result = self.visit(tree.children[1])
+        return result
+
+
+    def comparison(self, tree):
+        v1 = self.visit(tree.children[0]) 
+        op = tree.children[1].value      
+        v2 = self.visit(tree.children[2])  
+        if op == ">":
+            return v1 > v2
+        elif op == "<":
+            return v1 < v2
+        elif op == ">=":
+            return v1 >= v2
+        elif op == "<=":
+            return v1 <= v2
+        elif op == "==":
+            return v1 == v2
+        elif op == "!=":
+            return v1 != v2
